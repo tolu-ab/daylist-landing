90620ba347079dd99317b8daa06c1495503c9b30
Toluwanimi Alebiosu <alebiosutolu@gmail.com>
2026-08-03 13:20:19 -0400

    Add project configs, entry point, and page layout

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Hero from '../sections/Hero'
+import Demo from '../sections/Demo'
+import Closing from '../sections/Closing'
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Demo />
+      <Closing />
+    </main>
+  )
+}
